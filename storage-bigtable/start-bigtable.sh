d447bd3c168673af45d5801e457b098173c144c2
cosmin <cosmin.gamanusi@gmail.com>
2024-04-25 11:40:37 +0000

    SOL-12 : add docker-compose for quick local setup

---
+++ storage-bigtable/start-bigtable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Bigtable emulator started"
+
+gcloud beta emulators bigtable start --host-port=0.0.0.0:8086 &
+
+# Wait for the emulator to start
+sleep 3
+
+solana/storage-bigtable/init-bigtable.sh
+
+wait
