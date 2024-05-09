948d285b24dbd9920873f1a58e6d83e31b95975c
cosmin <cosmin.gamanusi@gmail.com>
2024-05-09 14:15:55 +0000

    first working sequencefile export

--- syncer/src/main/java/com/bwarelabs/App.java
+++ syncer/src/main/java/com/bwarelabs/App.java
@@ -1,67 +1,159 @@
 // original backup
 package com.bwarelabs;
 
+import com.google.common.collect.Lists;
+
 import com.google.cloud.bigtable.hbase.BigtableConfiguration;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.SequenceFile;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.serializer.WritableSerialization;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.fs.RawLocalFileSystem;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.KeyValue.Type;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.Cell;
+import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Result;
 import org.apache.hadoop.hbase.KeyValue;
 import org.apache.hadoop.hbase.KeyValueUtil;
+import org.apache.beam.sdk.options.ValueProvider.StaticValueProvider;
+import org.apache.beam.sdk.testing.SourceTestUtils;
+import org.apache.beam.sdk.values.KV;
+import org.apache.hadoop.hbase.mapreduce.ResultSerialization;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import org.apache.hadoop.io.SequenceFile.Writer;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
 
 public class App {
-  public static void main(String[] args) throws IOException {
-    // Bigtable Configuration
-    Configuration config = BigtableConfiguration.configure("emulator", "solana-ledger");
-    Connection connection = BigtableConfiguration.connect(config);
-    Table table = connection.getTable(TableName.valueOf("tx"));
+  public void testHBaseTypes() throws Exception {
+    final List<KV<ImmutableBytesWritable, Result>> data = Lists.newArrayList();
 
-    // Hadoop Configuration for SequenceFile
+    final int nRows = 10;
+    for (int i = 0; i < nRows; i++) {
+      String keyStr = String.format("%03d", i);
 
-    Configuration hadoopConfig = new Configuration();
+      ImmutableBytesWritable rowKey = new ImmutableBytesWritable(keyStr.getBytes());
 
-    Path path = new Path("file:///output/sequencefile/tx.seq");
-    ImmutableBytesWritable key = new ImmutableBytesWritable();
-    Result value = new Result();
+      @SuppressWarnings("deprecation")
+      Result value = Result.create(
+          Collections.singletonList(
+              CellUtil.createCell(
+                  keyStr.getBytes(),
+                  ("family").getBytes(),
+                  ("qualifier" + i).getBytes(),
+                  123456,
+                  Type.Put.getCode(),
+                  ("value" + i).getBytes())));
+      data.add(KV.of(rowKey, value));
+    }
+
+    // Write the file
+    // Configuration config = new Configuration(false);
+    Configuration config = new Configuration();
+    config.setStrings(
+        "io.serializations",
+        ResultSerialization.class.getName(),
+        WritableSerialization.class.getName());
 
     RawLocalFileSystem fs = new RawLocalFileSystem();
-    fs.setConf(hadoopConfig);
-    SequenceFile.Writer writer = null;
+    fs.setConf(config);
+    Path path = new Path("file:///output/sequencefile/dummy_table.seq");
 
+    Writer writer = null;
     try {
-      writer = SequenceFile.createWriter(hadoopConfig,
-          SequenceFile.Writer.file(fs.makeQualified(path)),
-          SequenceFile.Writer.keyClass(ImmutableBytesWritable.class),
-          SequenceFile.Writer.valueClass(ResultWritable.class),
-          SequenceFile.Writer.compression(SequenceFile.CompressionType.NONE));
+      writer = SequenceFile.createWriter(
+          config,
+          Writer.file(fs.makeQualified(path)),
+          Writer.keyClass(ImmutableBytesWritable.class),
+          Writer.valueClass(Result.class),
+          Writer.compression(SequenceFile.CompressionType.NONE));
 
-      Scan scan = new Scan();
-      ResultScanner scanner = table.getScanner(scan);
-
-      for (Result result : scanner) {
-        ImmutableBytesWritable rowKey = new ImmutableBytesWritable(result.getRow());
-
-        writer.append(rowKey, new ResultWritable(result));
+      for (KV<ImmutableBytesWritable, Result> kv : data) {
+        writer.append(kv.getKey(), kv.getValue());
       }
-
-    } finally
-
-    {
-      if (writer != null) {
-        writer.close();
-      }
-      table.close();
-      connection.close();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
+
+    if (writer != null) {
+      System.out.println("Closing writer...");
+      writer.close();
+    }
+
+  }
+
+  public static void main(String[] args) throws IOException {
+    // call testSimpleWritable
+    App app = new App();
+    System.out.println("Calling testHBaseTypes()...");
+    try {
+      app.testHBaseTypes();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    System.out.println("testHBaseTypes() done.");
+
+    return;
+
+    // Bigtable Configuration
+    // Configuration config = BigtableConfiguration.configure("emulator",
+    // "solana-ledger");
+    // Connection connection = BigtableConfiguration.connect(config);
+    // Table table = connection.getTable(TableName.valueOf("tx"));
+
+    // // Hadoop Configuration for SequenceFile
+
+    // Configuration hadoopConfig = new Configuration();
+
+    // Path path = new Path("file:///output/sequencefile/tx.seq");
+    // ImmutableBytesWritable key = new ImmutableBytesWritable();
+    // Result value = new Result();
+
+    // RawLocalFileSystem fs = new RawLocalFileSystem();
+    // fs.setConf(hadoopConfig);
+    // SequenceFile.Writer writer = null;
+
+    // try {
+    // writer = SequenceFile.createWriter(hadoopConfig,
+    // SequenceFile.Writer.file(fs.makeQualified(path)),
+    // SequenceFile.Writer.keyClass(Text.class),
+    // SequenceFile.Writer.valueClass(Text.class),
+    // SequenceFile.Writer.compression(SequenceFile.CompressionType.NONE));
+
+    // Scan scan = new Scan();
+    // ResultScanner scanner = table.getScanner(scan);
+
+    // for (Result result : scanner) {
+    // ImmutableBytesWritable rowKey = new ImmutableBytesWritable(result.getRow());
+
+    // writer.append(rowKey, new ResultWritable(result));
+    // }
+
+    // } finally
+
+    // {
+    // if (writer != null) {
+    // writer.close();
+    // }
+    // table.close();
+    // connection.close();
+    // }
   }
 }
