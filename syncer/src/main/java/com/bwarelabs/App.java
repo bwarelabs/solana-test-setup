5448e07fbbe8b67a595a0ea4f3905fbe6be01108
cosmin <cosmin.gamanusi@gmail.com>
2024-05-09 09:22:26 +0000

    added dataflow exported seqfile

--- syncer/src/main/java/com/bwarelabs/App.java
+++ syncer/src/main/java/com/bwarelabs/App.java
@@ -1,41 +1,39 @@
+// original backup
 package com.bwarelabs;
+
 import com.google.cloud.bigtable.hbase.BigtableConfiguration;
-import com.google.cloud.bigtable.hbase.BigtableOptionsFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.SequenceFile;
-import org.apache.hadoop.io.Text;
-import org.apache.hadoop.io.Writable;
-import org.apache.hadoop.io.LongWritable;
-import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
 import org.apache.hadoop.fs.RawLocalFileSystem;
-import org.apache.hadoop.io.compress.CompressionCodec;
-import org.apache.hadoop.io.compress.DefaultCodec;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.KeyValueUtil;
 
 import java.io.IOException;
 
-public class App 
-{
+public class App {
   public static void main(String[] args) throws IOException {
     // Bigtable Configuration
     Configuration config = BigtableConfiguration.configure("emulator", "solana-ledger");
-    //config.set(BigtableOptionsFactory.BIGTABLE_EMULATOR_HOST_KEY, "localhost:8086");
     Connection connection = BigtableConfiguration.connect(config);
-    Table table = connection.getTable(TableName.valueOf("blocks"));
+    Table table = connection.getTable(TableName.valueOf("tx"));
 
     // Hadoop Configuration for SequenceFile
 
     Configuration hadoopConfig = new Configuration();
-    Path path = new Path("file:///output/output.seq");
-    Text key = new Text();
-    LongWritable value = new LongWritable();
+
+    Path path = new Path("file:///output/sequencefile/tx.seq");
+    ImmutableBytesWritable key = new ImmutableBytesWritable();
+    Result value = new Result();
+
     RawLocalFileSystem fs = new RawLocalFileSystem();
     fs.setConf(hadoopConfig);
     SequenceFile.Writer writer = null;
@@ -43,20 +41,22 @@ public class App
     try {
       writer = SequenceFile.createWriter(hadoopConfig,
           SequenceFile.Writer.file(fs.makeQualified(path)),
-          SequenceFile.Writer.keyClass(Text.class),
-          SequenceFile.Writer.valueClass(LongWritable.class),
+          SequenceFile.Writer.keyClass(ImmutableBytesWritable.class),
+          SequenceFile.Writer.valueClass(ResultWritable.class),
           SequenceFile.Writer.compression(SequenceFile.CompressionType.NONE));
 
-      // Scan the table
       Scan scan = new Scan();
       ResultScanner scanner = table.getScanner(scan);
+
       for (Result result : scanner) {
-        // Assuming you are storing the rows as 'long' values and the key as text
-        key.set(new Text(result.getRow()));
-        value.set(Bytes.toLong(result.getValue(Bytes.toBytes("cf"), Bytes.toBytes("qualifier"))));
-        writer.append(key, value);
+        ImmutableBytesWritable rowKey = new ImmutableBytesWritable(result.getRow());
+
+        writer.append(rowKey, new ResultWritable(result));
       }
-    } finally {
+
+    } finally
+
+    {
       if (writer != null) {
         writer.close();
       }
