3ee9353c3f7aa1139ed77434b50a4355304984eb
cosmin <cosmin.gamanusi@gmail.com>
2024-05-13 12:18:59 +0000

    added checksum

--- syncer/src/main/java/com/bwarelabs/App.java
+++ syncer/src/main/java/com/bwarelabs/App.java
@@ -1,30 +1,28 @@
 package com.bwarelabs;
 
-import com.google.common.collect.Lists;
-
 import com.google.cloud.bigtable.hbase.BigtableConfiguration;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.SequenceFile;
 import org.apache.hadoop.io.serializer.WritableSerialization;
 import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.io.hfile.ChecksumUtil;
 import org.apache.hadoop.fs.RawLocalFileSystem;
+import org.apache.hadoop.hbase.CellScanner;
+import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
-import org.apache.hadoop.hbase.KeyValue.Type;
 import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.client.Scan;
-import org.apache.hadoop.hbase.CellUtil;
 import org.apache.hadoop.hbase.client.ResultScanner;
 import org.apache.hadoop.hbase.client.Result;
-import org.apache.beam.sdk.values.KV;
 import org.apache.hadoop.hbase.mapreduce.ResultSerialization;
 
 import org.apache.hadoop.io.SequenceFile.Writer;
 
 import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
+import java.security.NoSuchAlgorithmException;
 
 public class App {
 
@@ -38,20 +36,22 @@ public class App {
     App app = new App();
     try {
       app.writeSequenceFileFromTable(connection, "blocks");
-      app.writeSequenceFileFromTable(connection, "entries");
-      app.writeSequenceFileFromTable(connection, "tx");
-      app.writeSequenceFileFromTable(connection, "tx-by-addr");
+      // app.writeSequenceFileFromTable(connection, "entries");
+      // app.writeSequenceFileFromTable(connection, "tx");
+      // app.writeSequenceFileFromTable(connection, "tx-by-addr");
+
+      app.readDataAndCalculateChecksum("blocks");
     } catch (IOException e) {
       e.printStackTrace();
+    } finally {
+      connection.close();
     }
-
-    connection.close();
 
     System.out.println("Done!");
     return;
   }
 
-  public void writeSequenceFileFromTable(Connection connection, String tableName) throws IOException {
+  private void writeSequenceFileFromTable(Connection connection, String tableName) throws IOException {
     System.out.println("Writing SequenceFile from table: " + tableName);
 
     Table table = connection.getTable(TableName.valueOf(tableName));
@@ -70,11 +70,11 @@ public class App {
     Writer writer = null;
 
     try {
-      writer = SequenceFile.createWriter(hadoopConfig,
-          SequenceFile.Writer.file(fs.makeQualified(path)),
-          SequenceFile.Writer.keyClass(ImmutableBytesWritable.class),
-          SequenceFile.Writer.valueClass(Result.class),
-          SequenceFile.Writer.compression(SequenceFile.CompressionType.NONE));
+      // writer = SequenceFile.createWriter(hadoopConfig,
+      // SequenceFile.Writer.file(fs.makeQualified(path)),
+      // SequenceFile.Writer.keyClass(ImmutableBytesWritable.class),
+      // SequenceFile.Writer.valueClass(Result.class),
+      // SequenceFile.Writer.compression(SequenceFile.CompressionType.NONE));
 
       Scan scan = new Scan();
       ResultScanner scanner = table.getScanner(scan);
@@ -83,7 +83,10 @@ public class App {
       for (Result result : scanner) {
         numberOfRows++;
         ImmutableBytesWritable rowKey = new ImmutableBytesWritable(result.getRow());
-        writer.append(rowKey, result);
+        // writer.append(rowKey, result);
+
+        int checksum = calculateByteAdditionChecksum(result);
+        System.out.println("Checksum for row " + numberOfRows + ": " + checksum);
       }
 
       System.out.println("Number of rows written: " + numberOfRows);
@@ -93,67 +96,46 @@ public class App {
     {
       if (writer != null) {
         System.out.println("Closing writer...");
-        writer.close();
+        // writer.close();
       }
       table.close();
     }
   }
 
-  public void testHBaseTypes() throws Exception {
-    final List<KV<ImmutableBytesWritable, Result>> data = Lists.newArrayList();
-
-    final int nRows = 10;
-    for (int i = 0; i < nRows; i++) {
-      String keyStr = String.format("%03d", i);
-
-      ImmutableBytesWritable rowKey = new ImmutableBytesWritable(keyStr.getBytes());
-
-      @SuppressWarnings("deprecation")
-      Result value = Result.create(
-          Collections.singletonList(
-              CellUtil.createCell(
-                  keyStr.getBytes(),
-                  ("family").getBytes(),
-                  ("qualifier" + i).getBytes(),
-                  123456,
-                  Type.Put.getCode(),
-                  ("value" + i).getBytes())));
-
-      data.add(KV.of(rowKey, value));
-    }
-
-    // Write the file
-    // Configuration config = new Configuration(false);
-    Configuration config = new Configuration();
-    config.setStrings(
-        "io.serializations",
-        ResultSerialization.class.getName(),
-        WritableSerialization.class.getName());
-
-    RawLocalFileSystem fs = new RawLocalFileSystem();
-    fs.setConf(config);
-    Path path = new Path("file:///output/sequencefile/dummy_table.seq");
-
-    Writer writer = null;
-    try {
-      writer = SequenceFile.createWriter(
-          config,
-          Writer.file(fs.makeQualified(path)),
-          Writer.keyClass(ImmutableBytesWritable.class),
-          Writer.valueClass(Result.class),
-          Writer.compression(SequenceFile.CompressionType.NONE));
-
-      for (KV<ImmutableBytesWritable, Result> kv : data) {
-        writer.append(kv.getKey(), kv.getValue());
+  private int calculateByteAdditionChecksum(Result result) throws IOException {
+    int checksum = 0;
+    CellScanner scanner = result.cellScanner();
+    while (scanner.advance()) {
+      byte[] value = scanner.current().getValueArray();
+      for (byte b : value) {
+        checksum += b;
       }
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
 
-    if (writer != null) {
-      System.out.println("Closing writer...");
-      writer.close();
     }
+    return checksum;
+  }
 
+  private void readDataAndCalculateChecksum(String tableName) throws IOException {
+    System.out.println("Reading data from table: " + tableName);
+    // Set up HBase configuration
+    org.apache.hadoop.conf.Configuration config = HBaseConfiguration.create();
+    config.set("hbase.zookeeper.quorum", "hbase");
+    config.set("hbase.zookeeper.property.clientPort", "2181");
+
+    try (Connection connection = ConnectionFactory.createConnection(config);
+        Table table = connection.getTable(TableName.valueOf(tableName))) {
+
+      Scan scan = new Scan();
+      int numberOfRows = 0;
+      try (ResultScanner scanner = table.getScanner(scan)) {
+        for (Result result : scanner) {
+          int checksum = calculateByteAdditionChecksum(result);
+          System.out.println("Checksum for row: " + checksum);
+
+          numberOfRows++;
+        }
+      }
+      System.out.println("Number of rows read: " + numberOfRows);
+    }
   }
 }
